4c5e113fa638ef7b1e594028f5db632f5cd63e19
Isadora Ramos <isadorachb@gmail.com>
2024-03-13 20:15:03 -0300

    First commit
    Exercises 1.1 and 1.2 of Full Stack Open part 1.

---
+++ fullstackopen/part1/src/App.jsx
@@ -0,0 +1,61 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content 
+        part1={part1} 
+        part2={part2} 
+        part3={part3} 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}></Content>
+        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Total>
+    </div>
+  )
+}
+
+const Header = (props) =>{
+  return (
+    <>
+     <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) =>{
+  return (
+    <>
+      <Part name={props.part1} number={props.exercises1}></Part>
+      <Part name={props.part2} number={props.exercises2}></Part>
+      <Part name={props.part3} number={props.exercises3}></Part>
+    </>
+  )
+}
+
+const Part = (props) =>{
+  return (
+    <>
+      <p>
+          {props.name} {props.number}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) =>{
+  return (
+    <>
+     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>
+  )
+}
+
+export default App
