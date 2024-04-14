fd2acdae52b9794d44ee61f87f0a8713914adddd
Isadora Ramos <isadorachb@gmail.com>
2024-04-14 20:15:10 -0300

    Exercise 2.8

--- fullstackopen/part2/phonebook/src/App.jsx
+++ fullstackopen/part2/phonebook/src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone:''}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName, 
+      phone: newPhone
     };
     const exists = persons.filter(person => person.name === newName);
     console.log(exists.length)
     if(exists.length === 0){
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewPhone('')
     }else{
       alert(`${newName} is already added to phonebook`)
     }
@@ -26,21 +29,29 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value)
+    setNewPhone(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName} onChange={handleNameChange}>
+      <form onSubmit={addName}>
         <div>
-          name: <input value={newName} />
+          Name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          Number: <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(person => 
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>{person.name} - {person.phone}</p>
         )}
       </div>
     </div>
