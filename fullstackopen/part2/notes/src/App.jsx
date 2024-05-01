d453c58ea0b881f77f631101e07b08797edf5191
Isadora Ramos <isadorachb@gmail.com>
2024-04-30 23:27:47 -0300

    Change importance of note, add notes.js

--- fullstackopen/part2/notes/src/App.jsx
+++ fullstackopen/part2/notes/src/App.jsx
@@ -17,6 +17,16 @@ const App = () => {
       })
   }, [])
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
+  }
+
   const notesToShow = showAll? notes: notes.filter(note => note.important === true)
 
   const addNote = (event) => {
@@ -47,7 +57,8 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note toggleImportance={() => toggleImportanceOf(note.id)}
+          key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
