140c261da03399a60cf09e7c822b1f712e59b26c
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-05-05 16:45:15 -0300

    Add Notification and Footer components

--- fullstackopen/part2/notes/src/App.jsx
+++ fullstackopen/part2/notes/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import './index.css'
 import noteService from './services/notes'
 import Note from './components/Note.jsx'
+import Notification from './components/Notification.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
 
   const [notes, setNotes] = useState([]) 
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
      noteService
@@ -28,9 +31,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -62,6 +68,7 @@ const App = () => {
     <div>
       <h1>Notes</h1>
       <div>
+        <Notification message={errorMessage} />
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
@@ -76,6 +83,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>  
+      <Footer></Footer>
     </div>
   )
 }
