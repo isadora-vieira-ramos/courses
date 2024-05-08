9a50622032c695bc89a26fd0f5fa61be40891832
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-05-08 20:07:52 -0300

    Create custom middleware

--- fullstackopen/part3/examples/index.js
+++ fullstackopen/part3/examples/index.js
@@ -22,6 +22,22 @@ let notes = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
