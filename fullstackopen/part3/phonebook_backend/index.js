b917680cf47acdac8c74cc9c34a248f029618772
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-05-13 14:51:04 -0300

    Exercise 3.8

--- fullstackopen/part3/phonebook_backend/index.js
+++ fullstackopen/part3/phonebook_backend/index.js
@@ -33,6 +33,9 @@ morgan.token('host', function(req, res) {
     return req.hostname;
 });
 
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+
 app.get('/api/persons', (request, response) => {
   response.json(people)
 })
