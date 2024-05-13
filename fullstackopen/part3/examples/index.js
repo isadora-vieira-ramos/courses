6041aee0d5ce78ac2e7876b1bef5c8a0f4120f64
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-05-13 20:18:14 -0300

    Using express static

--- fullstackopen/part3/examples/index.js
+++ fullstackopen/part3/examples/index.js
@@ -1,8 +1,11 @@
 const http = require('http')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(cors())
 
 let notes = [
   {
@@ -91,7 +94,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
