5aa902d0d112e0482f2ac285ff25eca489920db4
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-06-03 16:25:51 -0300

    Exercise 3.13

--- fullstackopen/part3/phonebook_backend/index.js
+++ fullstackopen/part3/phonebook_backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
@@ -9,28 +11,6 @@ app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let people = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 morgan.token('host', function(req, res) {
     return req.hostname;
@@ -40,59 +20,61 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
 app.get('/api/persons', (request, response) => {
-  response.json(people)
+  Person.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-  const name = people.find(person => person.name.toUpperCase() == body.name.toUpperCase())
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body
+//   const name = people.find(person => person.name.toUpperCase() == body.name.toUpperCase())
 
-  if (!body || !body.number || !body.name) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+//   if (!body || !body.number || !body.name) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
 
-  if(name){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
+//   if(name){
+//     return response.status(400).json({ 
+//       error: 'name must be unique' 
+//     })
+//   }
 
-  const person = {
-    id: Math.floor(Math.random() * 10000),
-    number: body.number,
-    name: body.name,
-  }
+//   const person = {
+//     id: Math.floor(Math.random() * 10000),
+//     number: body.number,
+//     name: body.name,
+//   }
 
-  people = people.concat(person)
+//   people = people.concat(person)
 
-  response.json(person)
-})
+//   response.json(person)
+// })
 
-app.get('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id);
-    const person = people.find(person => person.id == id);
+// app.get('/api/persons/:id', (request, response) =>{
+//     const id = Number(request.params.id);
+//     const person = people.find(person => person.id == id);
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
+//     if (person) {
+//         response.json(person)
+//     } else {
+//         response.status(404).end()
+//     }
+// })
 
-app.get('/api/info', (request, response) => {
-  const count = people.length;
-  const date = new Date();
-  response.send(`<p>Phonebook has info for ${count} people</p></br><p>${date}</p>`)
-})
+// app.get('/api/info', (request, response) => {
+//   const count = people.length;
+//   const date = new Date();
+//   response.send(`<p>Phonebook has info for ${count} people</p></br><p>${date}</p>`)
+// })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  people = people.filter(people => people.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   people = people.filter(people => people.id !== id)
 
-  response.status(204).end()
-})
+//   response.status(204).end()
+// })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
