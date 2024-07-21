13edded5ea5873b413ef2946cc0cab0f5f28f26e
Isadora Vieira Ramos <isadorachb@gmail.com>
2024-07-21 20:51:03 -0300

    Change methods names for better clarity

--- fullstackopen/part2/phonebook/src/services/phonebook.js
+++ fullstackopen/part2/phonebook/src/services/phonebook.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 const baseUrl = '/api/persons'
 
-const getAll = () => {
+const getAllContacts = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-const create = newObject => {
+const createContact = newObject => {
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
 }
 
-const update = (id, newObject) => {
+const updateContact = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
 
-const remove = (id) => {
+const removeContact = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`)
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update, remove }
+export default { getAll: getAllContacts, create: createContact, update: updateContact, remove: removeContact }
